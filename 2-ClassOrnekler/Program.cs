88904463dd0011ac883917a59d4ad8eef8a6b32f
senasen2 <senasen2111@gmail.com>
2024-10-09 18:49:09 +0300

    Obcejt initilation

---
+++ 2-ClassOrnekler/Program.cs
@@ -0,0 +1,33 @@
+﻿namespace _2_ClassOrnekler
+{
+    internal class Program
+    {
+        //structlar stackte tutulurken, classlar heapte tutulur.
+        static void Main(string[] args)
+        {
+            #region Class Instance Alma
+            //1.YOL
+            Kitap kitap = new Kitap();
+            kitap.KitapAdi = "İnce Memed";
+            kitap.Yazar = "Yaşar Kemal";
+            kitap.BaskiYili = new DateOnly(1967,1,1);
+            kitap.SayfaSayisi = 360;
+            kitap.YayinEvi = "YKB";
+
+
+            //2.YOL OBJECT INITILATION
+            Kitap kitap2 = new Kitap()
+            {
+                Yazar = "Orhan Pamuk",
+                YayinEvi = "YKB",
+                KitapAdi = "Yeni Hayat",
+                BaskiYili = new DateOnly(2000, 1, 1),
+                SayfaSayisi = 250,
+                Barkod = "12556357"
+                
+            };
+
+            #endregion
+        }
+    }
+}
